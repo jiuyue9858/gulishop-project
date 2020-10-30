46c8dd56a42999ac14f5dd46aaf834796b375697
jiuyue <1550182624@qq.com>
2020-10-30 09:11:03 +0800

    第一天

--- src/router/index.js
+++ src/router/index.js
@@ -8,29 +8,35 @@ import Login from '@/views/Login'
 import Register from '@/views/Register'
 
 export default new VueRouter({
-    mode:'history',
-    routes:[
+    mode: 'history',
+    routes: [
         {
-            path:'/home',
-            component:Home
+            path: '/home',
+            component: Home
         },
         {
-            path:'/search/:keyword?',
-            component:Search,
-            name:'search',
-            props:(route) => ({keyword:route.params.keyword,keyword1:route.query.keyword1})
+            path: '/search/:keyword?',
+            component: Search,
+            name: 'search',
+            props: (route) => ({ keyword: route.params.keyword, keyword1: route.query.keyword1 })
         },
         {
-            path:'/login',
-            component:Login
+            path: '/login',
+            component: Login,
+            meta: {
+                isHidden: true
+            }
         },
         {
-            path:'/register',
-            component:Register
+            path: '/register',
+            component: Register,
+            meta: {
+                isHidden: true
+            }
         },
         {
-            path:'/',
-            redirect:'/home'
+            path: '/',
+            redirect: '/home'
         },
     ]
 })
